22db49b10f22ed104baedf6966a718bd640b360c
angelotadeucci <angelo_tadeucci@hotmail.com.br>
2019-10-18 18:45:00 -0300

    Initial commit

---
+++ src/classes/JogoDaForca.java
@@ -0,0 +1,131 @@
+package classes;
+
+import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.Normalizer;
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * @author angel
+ */
+public class JogoDaForca {
+
+    private String palavra;
+    private Character[] palavra2;
+    private int tentativas, erros;
+    private List<Character> letrasUsadas;
+
+    public JogoDaForca() throws NullPointerException {
+        this.letrasUsadas = new ArrayList<>();
+        this.palavra = Normalizer.normalize(lerPalavras(), Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "");
+        this.tentativas = 0;
+        this.erros = 0;
+        this.palavra2 = new Character[(palavra.length())];
+    }
+
+    public int getErros() {
+        return erros;
+    }
+
+    public void setErros(int erros) {
+        this.erros = erros;
+    }
+
+    public void addErro() {
+        this.erros += 1;
+    }
+
+    public Character[] getPalavra2() {
+        return palavra2;
+    }
+
+    public void addPalavra2(int pos, char letra) {
+        palavra2[pos] = letra;
+    }
+
+    private String lerPalavras() throws NullPointerException {
+        String fileName = "palavras.txt";
+        String resultado = null;
+        Random random = new Random();
+        int rng = random.nextInt(2396);
+        try {
+            FileReader fileReader = new FileReader(fileName);
+            try (BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+                do {
+                    for (int i = 0; i < rng; i++) {
+                        resultado = bufferedReader.readLine();
+                    }
+                    if (resultado == null) {
+                        break;
+                    }
+                    try {
+                        resultado = resultado.length() < 5 ? null : resultado;
+                    } catch (Exception e) {
+                        resultado = null;
+                    }
+                } while (resultado == null);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Arquivo não existe '" + fileName + "'");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return resultado;
+    }
+
+    public String getPalavra() {
+        return palavra;
+    }
+
+    public int getTentativas() {
+        return tentativas;
+    }
+
+    public void addTentativa() {
+        this.tentativas += 1;
+    }
+
+    public void setTentativa(int x) {
+        this.tentativas = x;
+    }
+
+    public boolean letraExisteNaPalavra(char x) {
+        boolean resultado = false;
+        //char[] arrayDaPalavra = palavra.toCharArray();
+        for (int i = 0; i < palavra.length(); i++) {
+            if (palavra.charAt(i) == x) {
+                return true;
+            }
+        }
+
+        return resultado;
+    }
+
+//    public char[] charArrayDaPalavra() {
+//        return palavra.toCharArray();
+//    }
+    public String getLetrasUsadas() {
+        StringBuilder reString = new StringBuilder();
+        for (int i = 0; i < letrasUsadas.toArray().length; i++) {
+            reString.append(letrasUsadas.toArray()[i]).append(" - ");
+        }
+        return reString.toString();
+    }
+
+    public Object[] getArrayLetrasUsadas() {
+        return letrasUsadas.toArray();
+    }
+
+    public boolean addLetrasUsadas(Character letra) {
+        if (!letrasUsadas.contains(letra)) {
+            letrasUsadas.add(letra);
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ src/tela/Tela.java
@@ -0,0 +1,383 @@
+package tela;
+
+import classes.JogoDaForca;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
+/**
+ * @author angel
+ */
+public class Tela extends javax.swing.JFrame {
+
+    private JogoDaForca forca;
+
+    public Tela() {
+        initComponents();
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setTitle("Jogo da Forca");
+        novoJogo();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jLImagem = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
+        jLogotipo = new javax.swing.JLabel();
+        jPanel3 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLPalavra = new javax.swing.JLabel();
+        jLLetrasUsadas = new javax.swing.JLabel();
+        jLPalpite = new javax.swing.JLabel();
+        jLErros = new javax.swing.JLabel();
+        jLDica = new javax.swing.JLabel();
+        jLDicaString = new javax.swing.JLabel();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMJogo = new javax.swing.JMenu();
+        jMINovoJogo = new javax.swing.JMenuItem();
+        jMISair = new javax.swing.JMenuItem();
+        jMAjuda = new javax.swing.JMenu();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLImagem.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/forca.jpg"))); // NOI18N
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLImagem)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLImagem)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        jLogotipo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/logotipo.jpg"))); // NOI18N
+        jLogotipo.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLogotipoMouseClicked(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLogotipo)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLogotipo)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        jPanel3.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Informações", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Dialog", 1, 12), new java.awt.Color(255, 0, 0))); // NOI18N
+
+        jLabel2.setText("Palavra:");
+
+        jLabel3.setText("Letras usadas:");
+
+        jLabel4.setText("Último palpite:");
+
+        jLabel5.setText("Erros restantes:");
+
+        jLPalavra.setText("jLabel6");
+
+        jLLetrasUsadas.setText("jLabel6");
+
+        jLPalpite.setText("jLabel6");
+
+        jLErros.setText("jLabel6");
+
+        jLDica.setText("Dica:");
+
+        jLDicaString.setText("jLabel7");
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel5)
+                    .addComponent(jLabel4)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel3Layout.createSequentialGroup()
+                        .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLLetrasUsadas)
+                            .addComponent(jLPalpite)
+                            .addComponent(jLErros))
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(jPanel3Layout.createSequentialGroup()
+                        .addComponent(jLPalavra)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jLDica)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLDicaString)
+                        .addGap(176, 176, 176))))
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(jLPalavra)
+                    .addComponent(jLDica)
+                    .addComponent(jLDicaString))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(jLLetrasUsadas))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(jLPalpite))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel5)
+                    .addComponent(jLErros))
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+
+        jMJogo.setText("Jogo");
+
+        jMINovoJogo.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_N, java.awt.event.InputEvent.CTRL_MASK));
+        jMINovoJogo.setText("Novo jogo");
+        jMINovoJogo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMINovoJogoActionPerformed(evt);
+            }
+        });
+        jMJogo.add(jMINovoJogo);
+
+        jMISair.setText("Sair");
+        jMISair.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMISairActionPerformed(evt);
+            }
+        });
+        jMJogo.add(jMISair);
+
+        jMenuBar1.add(jMJogo);
+
+        jMAjuda.setText("Ajuda");
+        jMAjuda.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMAjudaActionPerformed(evt);
+            }
+        });
+        jMenuBar1.add(jMAjuda);
+
+        setJMenuBar(jMenuBar1);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jLogotipoMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLogotipoMouseClicked
+        if (!jLogotipo.isEnabled()) {
+            return;
+        }
+        String input = JOptionPane.showInputDialog(this, "Digite uma letra", "Letra", 1);
+        if (input == null) {
+            return;
+        }
+        if (input.isEmpty()) {
+            JOptionPane.showMessageDialog(this, "Digite alguma letra!", "Aviso", 1);
+            return;
+        }
+        if (input.length() > 1) {
+            JOptionPane.showMessageDialog(this, "Digite somente uma letra!", "Aviso", 1);
+            return;
+        }
+        Character letra = input.charAt(0);
+        Object[] arraysDeLetras = forca.getArrayLetrasUsadas();
+        for (int i = 0; i < arraysDeLetras.length; i++) {
+            if (arraysDeLetras[i] == letra) {
+                JOptionPane.showMessageDialog(this, "Esta letra já foi usada!", "Erro", 0);
+                return;
+            }
+        }
+        forca.addLetrasUsadas(letra);
+        jLLetrasUsadas.setText(forca.getLetrasUsadas());
+        if (forca.letraExisteNaPalavra(letra)) {
+            for (int i = 0; i < forca.getPalavra().length(); i++) {
+                if (forca.getPalavra().charAt(i) == letra) {
+                    forca.addPalavra2(i, letra);
+                }
+            }
+            Character[] arrayDaPalavra2 = forca.getPalavra2();
+            String temp2 = "";
+            for (int i = 0; i < arrayDaPalavra2.length; i++) {
+                if (arrayDaPalavra2[i] == null) {
+                    temp2 += "_ ";
+                } else if (arrayDaPalavra2[i].toString() == " ") {
+                    temp2 += "  ";
+                } else if (arrayDaPalavra2[i].toString() == "-") {
+                    temp2 += "- ";
+                } else {
+                    temp2 += arrayDaPalavra2[i] + " ";
+                }
+            }
+            jLPalavra.setText(temp2);
+            jLPalpite.setText(letra + " (Correto)");
+        } else {
+            forca.addErro();
+            Icon icon = new ImageIcon(getClass().getClassLoader().getResource("imagens/forca" + (forca.getErros() + 1) + ".jpg"));
+            jLImagem.setIcon(icon);
+            jLPalpite.setText(letra + " (Errado)");
+        }
+        jLErros.setText("" + (-forca.getErros() + 6));
+        if (forca.getErros() == 6) {
+            JOptionPane.showMessageDialog(this, "Você perdeu!\nA palavra era: " + forca.getPalavra(), "Game over", 0);
+            jLogotipo.setEnabled(false);
+           // return;
+        }
+//        forca.addTentativa();
+//        if (forca.getTentativas() > 6) {
+//            JOptionPane.showMessageDialog(this, "Você perdeu!", "Game over", 0);
+//            jLogotipo.setEnabled(false);
+//        }
+    }//GEN-LAST:event_jLogotipoMouseClicked
+
+    private void jMINovoJogoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMINovoJogoActionPerformed
+        novoJogo();
+    }//GEN-LAST:event_jMINovoJogoActionPerformed
+
+    private void jMISairActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMISairActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_jMISairActionPerformed
+
+    private void jMAjudaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMAjudaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jMAjudaActionPerformed
+
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Tela.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(() -> {
+            new Tela().setVisible(true);
+        });
+        /*
+            java.awt.EventQueue.invokeLater(new Runnable() {
+              public void run() {
+                new Tela().setVisible(true);
+            }
+        });
+         */
+    }
+
+    //<editor-fold defaultstate="collapsed" desc="Variables">
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLDica;
+    private javax.swing.JLabel jLDicaString;
+    private javax.swing.JLabel jLErros;
+    private javax.swing.JLabel jLImagem;
+    private javax.swing.JLabel jLLetrasUsadas;
+    private javax.swing.JLabel jLPalavra;
+    private javax.swing.JLabel jLPalpite;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLogotipo;
+    private javax.swing.JMenu jMAjuda;
+    private javax.swing.JMenuItem jMINovoJogo;
+    private javax.swing.JMenuItem jMISair;
+    private javax.swing.JMenu jMJogo;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    // End of variables declaration//GEN-END:variables
+
+    //</editor-fold>
+    private void novoJogo() {
+        Icon icon = new ImageIcon(getClass().getClassLoader().getResource("imagens/forca.jpg"));
+        for (int i = 0; i < 10;) {
+            try {
+                forca = new JogoDaForca();
+                break;
+            } catch (NullPointerException ex) {
+                i++;
+            }
+        }
+        jLDica.setVisible(false);
+        jLDicaString.setVisible(false);
+        jLErros.setText("" + (-forca.getErros() + 6));
+        jLLetrasUsadas.setText(null);
+        jLPalpite.setText(null);
+        jLImagem.setIcon(icon);
+        jLogotipo.setEnabled(true);
+        String temp = "";
+        for (int i = 0; i < forca.getPalavra().length(); i++) {
+            temp += "_ ";
+        }
+        jLPalavra.setText(temp);
+    }
+}
